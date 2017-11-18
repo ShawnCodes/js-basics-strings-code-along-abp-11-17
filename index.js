3d8ab8a3c0149a93181d2430cd32d0f2fb852c22
shawn@huthunt.com <shawn@huthunt.com>
2017-11-18 08:56:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = "Samuel"
-const welcomeMessage = "Welcome to Flatbook, " + currentUser
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 const excitedWelcomeMessage = ""
 const shortGreeting = ""
