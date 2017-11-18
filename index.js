afd3939085c7bca39125c72bbe2ad77628948400
shawn@huthunt.com <shawn@huthunt.com>
2017-11-18 09:22:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = "Samuel"
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase(); + "!"
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(); 
 const shortGreeting = "Welcome, " + currentUser[0] + "!"
